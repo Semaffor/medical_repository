3d0694d795e6ddd573951e7826c6f5546d3116d7
Dima <dbuyvid@gmail.com>
2022-03-26 13:22:26 +0300

    Added WebApp config with HikariCP, i18n, thymeleaf implementations

---
+++ src/main/java/by/bsuir/app/config/WebAppConfig.java
@@ -0,0 +1,134 @@
+package by.bsuir.app.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan("by.bsuir.app")
+@EnableWebMvc
+@PropertySource("classpath:application.properties")
+public class WebAppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private final Environment env;
+    private ApplicationContext applicationContext;
+
+    private final static String PREFIX = "classpath:/templates/view/";
+    private final static String SUFFIX = ".html";
+    private final static String CHARACTER_ENCODING = "UTF-8";
+
+    public WebAppConfig(Environment environment, ApplicationContext applicationContext) {
+        this.env = environment;
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCacheable(false);
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setCharacterEncoding(CHARACTER_ENCODING);
+        templateResolver.setPrefix(PREFIX);
+        templateResolver.setSuffix(SUFFIX);
+        // HTML is the default value, added here for the sake of clarity.
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding(CHARACTER_ENCODING);
+        // 'order' and 'viewNames' are optional
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+
+    //----- Message externalization/internationalization -----//
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultEncoding(CHARACTER_ENCODING);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("ru"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    //----- Dispatcher configuration for serving static resources -----//
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").
+                addResourceLocations("classpath:/static/");
+
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("homePage");
+        registry.addViewController("/404.html").setViewName("404");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    //----- Flyway migration -----//
+
+}
